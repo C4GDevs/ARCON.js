f7274e07ed2613119feb0e8d976a4d5e992775fc
JonesBryan <jones.bryan171@gmail.com>
2022-06-09 14:17:17 -0400

    multipart packets

--- src/packetManager/Packet.ts
+++ src/packetManager/Packet.ts
@@ -6,12 +6,26 @@ export enum PacketTypes {
 
 export class Packet {
   public readonly type: PacketTypes;
+  public readonly sequence: number;
   public readonly data: string | null;
 
-  constructor(type: PacketTypes, data?: Buffer) {
+  constructor(type: PacketTypes, sequence: number, data?: Buffer | string) {
     this.type = type;
+    this.sequence = sequence;
     this.data = data?.toString() || null;
   }
 }
 
-export class MultiPartPacket extends Packet {}
+export class MultiPartPacket extends Packet {
+  public readonly index: number;
+  public readonly length: number;
+  public readonly data: string;
+
+  constructor(index: number, length: number, sequence: number, data: Buffer) {
+    super(PacketTypes.COMMAND, sequence, data);
+
+    this.index = index;
+    this.length = length;
+    this.data = data.toString();
+  }
+}
--- src/packetManager/PacketManager.ts
+++ src/packetManager/PacketManager.ts
@@ -1,11 +1,11 @@
 import crc32 from 'buffer-crc32';
-import { MultiPartPacket, Packet } from './Packet';
+import { MultiPartPacket, Packet, PacketTypes } from './Packet';
 
 export default class PacketManager {
-  private packetParts: Map<number, MultiPartPacket[]>;
+  private _packetParts: Map<number, MultiPartPacket[]>;
 
   constructor() {
-    this.packetParts = new Map();
+    this._packetParts = new Map();
   }
 
   public buildPacket(buf: Buffer) {
@@ -13,14 +13,46 @@ export default class PacketManager {
     const type = buf[7];
     const data = buf.subarray(8);
 
-    if (!this._validate(checksum, Buffer.from([type, ...data]))) {
+    if (this._validate(checksum, Buffer.from([0xff, type, ...data])) !== 0) {
       throw new Error('Invalid packet checksum');
     }
 
-    return new Packet(type, data);
+    const sequence = data[0];
+
+    // Packet contains multiple parts
+    if (data[1] === 0x00) {
+      const length = data[2];
+      const index = data[3];
+
+      const packet = new MultiPartPacket(index, length, sequence, data.subarray(4));
+
+      if (index === 0) {
+        this._packetParts.set(sequence, [packet]);
+        return packet;
+      }
+
+      const parts = this._packetParts.get(sequence);
+
+      if (!parts || !parts.length) throw new Error('Could not find MultiPartPacket index');
+
+      parts.push(packet);
+
+      // Construct whole packet
+      if (index === length - 1) {
+        const message = parts.map((p) => p.data).join('');
+
+        this._packetParts.delete(sequence);
+
+        return new Packet(PacketTypes.COMMAND, sequence, message);
+      }
+
+      return packet;
+    }
+
+    return new Packet(type, sequence, data);
   }
 
   private _validate(checksum: Buffer, data: Buffer) {
-    return checksum.compare(crc32(data.reverse()));
+    return checksum.compare(crc32(data).reverse());
   }
 }
