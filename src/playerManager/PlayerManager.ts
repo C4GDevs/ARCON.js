9febf64f1ebcb570257493417b049b9cee232780
JonesBryan <jones.bryan171@gmail.com>
2022-06-09 09:59:57 -0400

    docs

--- src/playerManager/PlayerManager.ts
+++ src/playerManager/PlayerManager.ts
@@ -1,36 +1,60 @@
-import ARcon from '../client';
 import Player from './Player';
 
+/**
+ * Data that resolves to a {@link Player} object. This can be:
+ * * A Player object
+ * * A BattlEye GUID
+ * * An IP address
+ */
 type PlayerResolvable = Player | string | number;
 
 export default class PlayerManager {
-  private readonly _arcon: ARcon;
   private readonly _cache: Set<Player>;
 
-  constructor(arcon: ARcon) {
+  constructor() {
     this._cache = new Set();
-    this._arcon = arcon;
   }
 
   get cache() {
     return this._cache;
   }
 
+  /**
+   * Add a player to the cache.
+   * @param player The player to add.
+   */
   public add(player: Player) {
     this._cache.add(player);
   }
 
+  /**
+   * Removes a player from the cache.
+   * @param player The player to remove.
+   */
   public remove(player: Player) {
     this._cache.delete(player);
   }
 
-  public resolve(player: PlayerResolvable) {
+  /**
+   * Resolves a {@link PlayerResolvable} to a {@link Player} object.
+   *
+   * @param player The player to find.
+   * @returns A {@link Player} object if found, else `null`.
+   *
+   * @example
+   * ```ts
+   * const player = resolve("77.125.33.126");
+   * ```
+   */
+  public resolve(player: PlayerResolvable): Player | null {
     if (player instanceof Player) return player;
 
     const playerList = [...this._cache];
 
-    if (typeof player === 'number') return playerList.find((p) => p.id === player);
-    if (/^[0-9a-z]{32}$/.test(player)) return playerList.find((p) => p.guid === player);
-    if (/^(?>\d{1,3}\.){3}\d{1,3}$/.test(player)) return playerList.find((p) => p.ip === player);
+    if (typeof player === 'number') return playerList.find((p) => p.id === player) || null;
+    if (/^[0-9a-z]{32}$/.test(player)) return playerList.find((p) => p.guid === player) || null;
+    if (/^(?>\d{1,3}\.){3}\d{1,3}$/.test(player)) return playerList.find((p) => p.ip === player) || null;
+
+    return null;
   }
 }
