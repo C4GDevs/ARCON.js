98cf566f0ba9fe92137e2636755d89de4f14211a
JonesBryan <jones.bryan171@gmail.com>
2022-08-21 14:44:58 -0400

    fix tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
-import { arcon } from '../src';
+import { Arcon } from '../src';
 
-const connection = new arcon({ ip: '140.82.114.3', port: 2333, password: 'test', timeout: 100 });
+const connection = new Arcon({ ip: '140.82.114.3', port: 2333, password: 'test', timeout: 100 });
 
 const successResponsePacket = Buffer.from([0x42, 0x45, 0x69, 0xdd, 0xde, 0x36, 0xff, 0x00, 0x01]);
 const failLoginPacket = Buffer.from([0x42, 0x45, 0xff, 0xed, 0xd9, 0x41, 0xff, 0x00, 0x00]);
