f270e37eaa10e5498390ef4c50e270d220d81d15
JonesBryan <jones.bryan171@gmail.com>
2022-08-25 02:30:27 -0400

    more tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,13 @@
 import { expect } from 'chai';
-import { Arcon } from '../src';
+import { Arcon, PacketTypes } from '../src';
 
-const connection = new Arcon({ ip: '140.82.114.3', port: 2333, password: 'test', timeout: 100 });
+const connection = new Arcon({
+  ip: '140.82.114.3',
+  port: 2333,
+  password: 'test',
+  timeout: 100,
+  separateMessageTypes: true
+});
 
 const successResponsePacket = Buffer.from([0x42, 0x45, 0x69, 0xdd, 0xde, 0x36, 0xff, 0x00, 0x01]);
 const failLoginPacket = Buffer.from([0x42, 0x45, 0xff, 0xed, 0xd9, 0x41, 0xff, 0x00, 0x00]);
@@ -30,5 +36,29 @@ describe('Connection', () => {
 
   it('Errors on multiple connections', async () => {
     connection.connect().catch((e) => expect(e).to.equal('Already connected to server'));
+  });
+
+  it('Handles messages', async () => {
+    const manager = connection['_packetManager'];
+    const connectionPacket = manager.buildBuffer(
+      PacketTypes.SERVER_MESSAGE,
+      'Player #0 Tren (37.158.225.96:9) connected'
+    );
+    const guidPacket = manager.buildBuffer(
+      PacketTypes.SERVER_MESSAGE,
+      'Verified GUID (fec4444cd6c294037444ca480f3f08de) of player #0 Tren'
+    );
+    const disconnectionPacket = manager.buildBuffer(PacketTypes.SERVER_MESSAGE, 'Player #0 Tren disconnected');
+
+    connection['_handlePacket'](connectionPacket);
+    expect(connection.playerManager.players.length).to.be.greaterThan(0);
+
+    connection['_handlePacket'](guidPacket);
+
+    expect(connection.playerManager.players[0].guid).not.to.be.undefined;
+
+    connection['_handlePacket'](disconnectionPacket);
+
+    expect(connection.playerManager.players.length).to.equal(0);
   });
 });
