689644f9dcb44614086dd9c5d2f605daac61d622
JonesBryan <jones.bryan171@gmail.com>
2022-12-25 03:25:06 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import Arcon from './Arcon';
 import { DisconnectInfo, ConnectionOptions } from './Arcon';
 import Player from './Arcon/Players/Player';
 
-export { Player, DisconnectInfo };
+export { Player, DisconnectInfo, ConnectionOptions };
 
 export default Arcon;
