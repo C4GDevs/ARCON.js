d1a7cc0e4e3cccc8ed7d3e308e9d6585c7e99b0d
JonesBryan <jones.bryan171@gmail.com>
2023-01-07 09:27:31 -0500

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import Arcon from './Arcon';
+import Arcon, { MessageChannels, PlayerMessageInfo } from './Arcon';
 import { DisconnectInfo, ConnectionOptions } from './Arcon';
 import Player from './Arcon/Players/Player';
 
-export { Player, DisconnectInfo, ConnectionOptions };
+export { Player, DisconnectInfo, ConnectionOptions, MessageChannels, PlayerMessageInfo };
 
 export default Arcon;
