04440a1c29ca8e4f7e1acc401d6de476f72a3a6b
JonesBryan <jones.bryan171@gmail.com>
2023-04-08 18:19:06 -0400

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import Arcon, { MessageChannels, PlayerMessageInfo } from './Arcon';
+import Arcon, { BELog, BELogTypes, MessageChannels, PlayerMessageInfo } from './Arcon';
 import { DisconnectInfo, ConnectionOptions } from './Arcon';
 import Player from './Arcon/Players/Player';
 
-export { Player, DisconnectInfo, ConnectionOptions, MessageChannels, PlayerMessageInfo };
+export { Player, DisconnectInfo, ConnectionOptions, MessageChannels, PlayerMessageInfo, BELogTypes, BELog };
 
 export default Arcon;
