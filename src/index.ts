b24ed3df03497a418549f64c6ab3c9544be221d5
JonesBryan <jones.bryan171@gmail.com>
2024-03-17 20:19:55 -0400

    reset

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import Arcon, { BELog, BELogTypes, MessageChannels, PlayerMessageInfo } from './Arcon';
-import { DisconnectInfo, ConnectionOptions } from './Arcon';
-import Player from './Arcon/Players/Player';
-
-export { Player, DisconnectInfo, ConnectionOptions, MessageChannels, PlayerMessageInfo, BELogTypes, BELog };
-
-export default Arcon;
